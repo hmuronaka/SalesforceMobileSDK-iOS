bead0c05cf944de9cc70d434f24a758b604a3d6a
hmuronaka <hmuronaka@terrasky.co.jp>
2020-03-02 17:07:24 +0900

    for test

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFUserAccountManager.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/Security/SFUserAccountManager.m
@@ -57,6 +57,7 @@
 #import "SFPasscodeManager.h"
 #import "SFNetwork.h"
 #import "SFSDKSalesforceAnalyticsManager.h"
+#import "SFSDKAuthHelper.h"
 
 // Notifications
 NSNotificationName SFUserAccountManagerDidChangeUserNotification       = @"SFUserAccountManagerDidChangeUserNotification";
@@ -1865,6 +1866,7 @@ static NSString * const kSFGenericFailureAuthErrorHandler = @"GenericFailureErro
 }
 
 - (void)presentLoginView:(SFSDKAuthViewHolder *)viewHandler {
+    [SFSDKAuthHelper append:@"presentLoginView"];
    
     [[SFSDKWindowManager sharedManager].authWindow presentWindow];
     void (^presentViewBlock)(void) = ^void() {
